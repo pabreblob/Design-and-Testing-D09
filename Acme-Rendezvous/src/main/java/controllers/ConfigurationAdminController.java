c3d020bc9a4730350f66bedc2f6e0bcd39699713
Daniel Carpio <danielcarpioc@gmail.com>
2018-03-05 11:14:21 +0100

    Añadido el controller de Configuration

---
+++ Acme-Rendezvous/src/main/java/controllers/ConfigurationAdminController.java
@@ -0,0 +1,32 @@
+
+package controllers;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.ModelAndView;
+
+import services.ConfigurationService;
+import domain.Configuration;
+
+@Controller
+@RequestMapping("configuration/admin")
+public class ConfigurationAdminController extends AbstractController {
+
+	@Autowired
+	private ConfigurationService	configurationService;
+
+
+	public ConfigurationAdminController() {
+		super();
+	}
+
+	@RequestMapping("/display")
+	public ModelAndView display() {
+		final ModelAndView res = new ModelAndView("configuration/display");
+		final Configuration conf = this.configurationService.find();
+		res.addObject("config", conf);
+		return res;
+	}
+
+}
