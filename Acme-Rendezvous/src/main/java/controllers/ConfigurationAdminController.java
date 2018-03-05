48cd1d9cf79144e78c503c53f8dc3e9f00d25177
Daniel Carpio <danielcarpioc@gmail.com>
2018-03-05 12:19:51 +0100

    Editado el controlador de configuration para implementar la edición

--- Acme-Rendezvous/src/main/java/controllers/ConfigurationAdminController.java
+++ Acme-Rendezvous/src/main/java/controllers/ConfigurationAdminController.java
@@ -3,7 +3,10 @@ package controllers;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.util.Assert;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
 import org.springframework.web.servlet.ModelAndView;
 
 import services.ConfigurationService;
@@ -26,6 +29,33 @@ public class ConfigurationAdminController extends AbstractController {
 		final ModelAndView res = new ModelAndView("configuration/list");
 		final Configuration conf = this.configurationService.find();
 		res.addObject("config", conf);
+		return res;
+	}
+
+	@RequestMapping("/edit")
+	public ModelAndView edit() {
+		final ModelAndView res = new ModelAndView("configuration/edit");
+		final Configuration conf = this.configurationService.find();
+		res.addObject("configuration", conf);
+		return res;
+	}
+
+	@RequestMapping(value = "/save", method = RequestMethod.POST)
+	public ModelAndView save(final Configuration configuration, final BindingResult binding) {
+		ModelAndView res;
+		if (binding.hasErrors()) {
+			res = new ModelAndView("configuration/edit");
+			res.addObject("configuration", configuration);
+		} else
+			try {
+				Assert.notNull(configuration);
+				this.configurationService.save(configuration);
+				res = new ModelAndView("redirect:list.do");
+			} catch (final Throwable oops) {
+				res = new ModelAndView("configuration/edit");
+				res.addObject("configuration", configuration);
+				res.addObject("message", "configuration.error");
+			}
 		return res;
 	}
 
