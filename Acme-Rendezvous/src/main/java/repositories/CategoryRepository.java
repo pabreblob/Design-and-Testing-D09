3594b3fd92d2dc3f5c057e8f76f02f6a6b697966
Pablo Narváez <pabnarsie@alum.us.es>
2018-03-05 11:22:40 +0100

    Añadido repositorio de category

---
+++ Acme-Rendezvous/src/main/java/repositories/CategoryRepository.java
@@ -0,0 +1,10 @@
+
+package repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import domain.Category;
+
+public interface CategoryRepository extends JpaRepository<Category, Integer> {
+
+}
