a52ef3f37071d6fa8e4542885eae583f6cf6889b
Daniel Carpio <danielcarpioc@gmail.com>
2018-03-08 09:50:39 +0100

    Solucionado problema de importación en Manager

--- Acme-Rendezvous/src/main/java/domain/Manager.java
+++ Acme-Rendezvous/src/main/java/domain/Manager.java
@@ -8,6 +8,7 @@ import javax.persistence.AccessType;
 import javax.persistence.Entity;
 import javax.persistence.OneToMany;
 import javax.validation.constraints.NotNull;
+import javax.validation.constraints.Pattern;
 
 import org.hibernate.validator.constraints.NotBlank;
 import org.hibernate.validator.constraints.SafeHtml;
