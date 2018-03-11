a2054bf04dc2a7d9c42d79487d875da2efb257b9
raffrearaUS <raffrearaus@gmail.com>
2018-03-11 12:33:29 +0100

    Actualizados los tests de Announcement e incluido Javadoc

--- Acme-Rendezvous/src/test/java/services/AnnouncementTest.java
+++ Acme-Rendezvous/src/test/java/services/AnnouncementTest.java
@@ -15,7 +15,6 @@ import org.springframework.util.Assert;
 import utilities.AbstractTest;
 import domain.Announcement;
 import domain.Rendezvous;
-import domain.User;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(locations = {
@@ -31,6 +30,11 @@ public class AnnouncementTest extends AbstractTest {
 	private UserService			userService;
 
 
+	/**
+	 * Tests the creation of announcements.
+	 * <p>
+	 * This method is used to test the creation of empty announcements before passing them to the corresponding views.
+	 */
 	@Test
 	public void testCreateAnnouncement() {
 		super.authenticate("user1");
@@ -39,64 +43,202 @@ public class AnnouncementTest extends AbstractTest {
 		super.authenticate(null);
 	}
 
+	/**
+	 * Tests the saving of announcements.
+	 * <p>
+	 * This method tests the creation and later saving of announcements as it would be done by an user in the corresponding views.
+	 */
 	@Test
-	public void testSaveAnnouncement() {
-		super.authenticate("user1");
-		final Announcement res = this.announcementService.create();
-		res.setTitle("titletest");
-		res.setDescription("desctest");
-		res.setRendezvous(new ArrayList<Rendezvous>(this.userService.findByPrincipal().getCreatedRendezvous()).get(0));
-		final Announcement a = this.announcementService.save(res);
-		final Announcement found = this.announcementService.findOne(a.getId());
-		Assert.notNull(a);
-		Assert.isTrue(found.equals(a));
-		super.authenticate(null);
+	public void driverSaveAnnouncement() {
+		final Object testingData[][] = {
+			{
+				"user1", null
+			}, {
+				"user2", null
+			}, {
+				null, IllegalArgumentException.class
+			}
+		};
+		for (int i = 0; i < testingData.length; i++)
+			this.templateSave((String) testingData[i][0], (Class<?>) testingData[i][1]);
 	}
 
-	@Test
-	public void testFindOneAnnouncement() {
-		super.authenticate("user1");
-		final Announcement res = this.announcementService.create();
-		res.setTitle("titletest");
-		res.setDescription("desctest");
-		res.setRendezvous(new ArrayList<Rendezvous>(this.userService.findByPrincipal().getCreatedRendezvous()).get(0));
-		final Announcement a = this.announcementService.save(res);
-		final Announcement found = this.announcementService.findOne(a.getId());
-		Assert.notNull(a);
-		Assert.isTrue(found.equals(a));
-		super.authenticate(null);
+	/**
+	 * Template for testing the saving of announcements.
+	 * <p>
+	 * This method defines the template used for the tests that check the saving of announcements.
+	 * 
+	 * @param username
+	 *            The username of the user that logs in. Use <code>null</code> if no user should be logged in.
+	 * @param expected
+	 *            The expected exception to be thrown. Use <code>null</code> if no exception is expected.
+	 */
+	protected void templateSave(final String username, final Class<?> expected) {
+		Class<?> caught;
+		caught = null;
+		try {
+			super.authenticate(username);
+			final Announcement res = this.announcementService.create();
+			res.setTitle("titletest");
+			res.setDescription("desctest");
+			res.setRendezvous(new ArrayList<Rendezvous>(this.userService.findByPrincipal().getCreatedRendezvous()).get(0));
+			final Announcement a = this.announcementService.save(res);
+			final Announcement found = this.announcementService.findOne(a.getId());
+			Assert.notNull(a);
+			Assert.isTrue(found.equals(a));
+			super.authenticate(null);
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		}
+		this.checkExceptions(expected, caught);
 	}
 
+	/**
+	 * Tests the finding of one announcement.
+	 * <p>
+	 * This method checks that an announcement can be saved and later correctly found.
+	 */
 	@Test
-	public void testDeleteAnnouncement() {
-		super.authenticate("user1");
-		final Announcement res = this.announcementService.create();
-		res.setTitle("titletest");
-		res.setDescription("desctest");
-		res.setRendezvous(new ArrayList<Rendezvous>(this.userService.findByPrincipal().getCreatedRendezvous()).get(0));
-		final Announcement a = this.announcementService.save(res);
-		this.announcementService.delete(a);
-		final Announcement found = this.announcementService.findOne(a.getId());
-		Assert.notNull(a);
-		Assert.isTrue(found == null);
-		super.authenticate(null);
+	public void driverFindOneAnnouncement() {
+		final Object testingData[][] = {
+			{
+				"user1", null
+			}, {
+				"user2", null
+			}, {
+				null, IllegalArgumentException.class
+			}
+		};
+		for (int i = 0; i < testingData.length; i++)
+			this.templateFindOne((String) testingData[i][0], (Class<?>) testingData[i][1]);
 	}
 
-	@Test
-	public void testFindByPrincipal() {
-		super.authenticate("user1");
-		final User principal = this.userService.findByPrincipal();
-		final User aux = this.userService.findOne(principal.getId());
-		Assert.isTrue(principal.equals(aux));
+	/**
+	 * Template for testing the finding of one announcement.
+	 * <p>
+	 * This method defines the template used for the tests that check the finding of one announcement.
+	 * 
+	 * @param username
+	 *            The username of the user that logs in. Use <code>null</code> if no user should be logged in.
+	 * @param expected
+	 *            The expected exception to be thrown. Use <code>null</code> if no exception is expected.
+	 */
+	protected void templateFindOne(final String username, final Class<?> expected) {
+		Class<?> caught;
+		caught = null;
+		try {
+			super.authenticate(username);
+			final Announcement res = this.announcementService.create();
+			res.setTitle("titletest");
+			res.setDescription("desctest");
+			res.setRendezvous(new ArrayList<Rendezvous>(this.userService.findByPrincipal().getCreatedRendezvous()).get(0));
+			final Announcement a = this.announcementService.save(res);
+			final Announcement found = this.announcementService.findOne(a.getId());
+			Assert.notNull(a);
+			Assert.isTrue(found.equals(a));
+			super.authenticate(null);
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		}
+		this.checkExceptions(expected, caught);
 	}
 
+	/**
+	 * Tests the deletion of an announcement.
+	 * <p>
+	 * This method tests that an announcement can be saved and later deleted, and checks that it cannot be found.
+	 */
 	@Test
-	public void testFindAnnouncementsByUserId() {
-		super.authenticate("user1");
-		final Collection<Announcement> res = this.announcementService.findAnnouncementsByPrincipal();
-		Assert.notNull(res);
-		Assert.isTrue(res.size() > 0);
-		super.authenticate(null);
+	public void driverDeleteAnnouncement() {
+		final Object testingData[][] = {
+			{
+				"user1", null
+			}, {
+				"user2", null
+			}, {
+				null, IllegalArgumentException.class
+			}
+		};
+		for (int i = 0; i < testingData.length; i++)
+			this.templateDelete((String) testingData[i][0], (Class<?>) testingData[i][1]);
+	}
+
+	/**
+	 * Template for testing the deletion of an announcement.
+	 * <p>
+	 * This method defines the template used to test the deletion of an announcement.
+	 * 
+	 * @param username
+	 *            The username of the user that logs in. Use <code>null</code> if no user should be logged in.
+	 * @param expected
+	 *            The expected exception to be thrown. Use <code>null</code> if no exception is expected.
+	 */
+	protected void templateDelete(final String username, final Class<?> expected) {
+		Class<?> caught;
+		caught = null;
+		try {
+			super.authenticate(username);
+			final Announcement res = this.announcementService.create();
+			res.setTitle("titletest");
+			res.setDescription("desctest");
+			res.setRendezvous(new ArrayList<Rendezvous>(this.userService.findByPrincipal().getCreatedRendezvous()).get(0));
+			final Announcement a = this.announcementService.save(res);
+			this.announcementService.delete(a);
+			final Announcement found = this.announcementService.findOne(a.getId());
+			Assert.notNull(a);
+			Assert.isTrue(found == null);
+			super.authenticate(null);
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		}
+		this.checkExceptions(expected, caught);
+	}
+
+	/**
+	 * Tests the listing of announcements.
+	 * <p>
+	 * This method tests the listing of the announcements that a user has created.
+	 */
+	@Test
+	public void driverFindAnnouncementsByUserId() {
+		final Object testingData[][] = {
+			{
+				"user1", 1, null
+			}, {
+				"user2", 0, null
+			}, {
+				null, 0, IllegalArgumentException.class
+			}
+		};
+		for (int i = 0; i < testingData.length; i++)
+			this.templateFindByUserId((String) testingData[i][0], (int) testingData[i][1], (Class<?>) testingData[i][2]);
+	}
+
+	/**
+	 * Template for testing the listing of announcements.
+	 * <p>
+	 * This method defines the template used to test the listing of announcements. Note that the announcements are retrieved from the currently logged in user.
+	 * 
+	 * @param username
+	 *            The username of the user that logs in. Use <code>null</code> if no user should be logged in.
+	 * @param minimumLength
+	 *            The minimum expected length of the list of announcements that the user has created.
+	 * @param expected
+	 *            The expected exception to be thrown. Use <code>null</code> if no exception is expected.
+	 */
+	protected void templateFindByUserId(final String username, final int minimumLength, final Class<?> expected) {
+		Class<?> caught;
+		caught = null;
+		try {
+			super.authenticate(username);
+			final Collection<Announcement> res = this.announcementService.findAnnouncementsByPrincipal();
+			Assert.notNull(res);
+			Assert.isTrue(res.size() >= minimumLength);
+			super.authenticate(null);
+		} catch (final Throwable oops) {
+			caught = oops.getClass();
+		}
+		this.checkExceptions(expected, caught);
 	}
 
 }
