00c227befdccbfad1da59f70651f79ede242f650
Daniel <danielcarpioc@gmail.com>
2018-03-18 12:13:22 +0100

    Subido el Test de Configuration

--- Acme-Rendezvous/src/main/java/services/ConfigurationService.java
+++ Acme-Rendezvous/src/main/java/services/ConfigurationService.java
@@ -23,6 +23,17 @@ public class ConfigurationService {
 
 	public Configuration save(final Configuration configuration) {
 		Assert.notNull(configuration);
+		Assert.isTrue(!configuration.getBannerUrl().equals(""));
+		Assert.notNull(configuration.getBannerUrl());
+		Assert.isTrue(!configuration.getCurrency().equals(""));
+		Assert.notNull(configuration.getCurrency());
+		Assert.isTrue(!configuration.getBusinessName().equals(""));
+		Assert.notNull(configuration.getBusinessName());
+		Assert.isTrue(!configuration.getWelcomeEng().equals(""));
+		Assert.notNull(configuration.getWelcomeEng());
+		Assert.isTrue(!configuration.getWelcomeEsp().equals(""));
+		Assert.notNull(configuration.getWelcomeEsp());
+
 		return this.configurationRepository.save(configuration);
 	}
 
